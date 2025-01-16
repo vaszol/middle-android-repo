c1dd8940bc2d2afe0d9f78818089d603355db72b
vasiliy <vaszol@mail.ru>
2025-01-16 21:31:35 +0300

    Второй экран скомпонован с помощью Jetpack Compose.

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.example.androidpracticumcustomview.R
 
 /*
 Задание:
@@ -24,12 +28,10 @@ fun MainScreen() {
 
             CustomContainerCompose(
                 firstChild = {
-                    // TODO
-                    // ...
+                    Text(stringResource(R.string.hello), fontSize = 30f.sp)
                 },
                 secondChild = {
-                    // TODO
-                    // ...
+                    Text(stringResource(R.string.world), fontSize = 30f.sp)
                 }
             )
         }
